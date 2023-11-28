3945f4932b94e80fee487e78a8ce8bddc659d146
apvazquez86 <37312405+apvazquez86@users.noreply.github.com>
2023-11-28 00:07:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Oliverrrrrr y Betty!!!!!!!!" }');
+    res.send('{ "response": "Oliverrrrrr,Betty y Arcel!!!!!!!!!!!" }');
 });
 
 app.get('/will', function (req, res) {
